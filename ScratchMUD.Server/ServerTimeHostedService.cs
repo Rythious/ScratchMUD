38f7743692d737471980183d1edfb26b27c1068e
Justin Morgan <Rythious@gmail.com>
2019-04-06 16:02:22 -0400

    Created ServerTimeHostedService for heartbeat
    Closes #2.  Created a hosted service that triggers a call to the clients every minute with the server time.  This was my first use of IHubContext and was slightly painful to get working until @bofirial guided me to hosted services.

---
+++ ScratchMUD.Server/ServerTimeHostedService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+using ScratchMUD.Server.Hubs;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScratchMUD.Server
+{
+    public class ServerTimeHostedService : IHostedService, IDisposable
+    {
+        private readonly IHubContext<EventHub> _hubContext;
+        private Timer _timer;
+
+        public ServerTimeHostedService(IHubContext<EventHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async void TrackMinutes(object state)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", DateTime.Now.ToString());
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(TrackMinutes, null, TimeSpan.FromMilliseconds(1000 - DateTime.Now.Millisecond), TimeSpan.FromMinutes(1));
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
