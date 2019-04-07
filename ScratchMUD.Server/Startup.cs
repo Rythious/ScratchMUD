deae1d9026c83f00de883f284953a75390434900
Justin Morgan <Rythious@gmail.com>
2019-04-06 21:07:57 -0400

    Pulled in Bootstrap 4 and updated MUD output
    Closes #5. Pulled in Bootstrap explicitly even though it was already in the project by CDN reference.  Updated the client  output window so that it resizes according to browser width.

--- ScratchMUD.Server/Startup.cs
+++ ScratchMUD.Server/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ScratchMUD.Server.Hubs;
